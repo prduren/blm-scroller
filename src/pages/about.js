e86b5a846d3a84278e0b47a84cd9751486155bf0
pduren <57735971+kParkz@users.noreply.github.com>
2020-06-04 11:26:12 -0500

    about/donate skeleton

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 
 const AboutPage = () => {
-  return <div>about page!!!!</div>;
+  return (
+    <div style={{ padding: 24, textAlign: "center" }}>
+      <Typography variant="h1">ABOUT</Typography>
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/pages/donate.js
+++ src/pages/donate.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
+import Link from "../components/Link";
 
 const DonatePage = () => {
-  return <div>donation station!!!!</div>;
+  return (
+    <div style={{ padding: 24, textAlign: "center" }}>
+      {" "}
+      <Typography variant="h1">DONATE</Typography>
+      <Typography variant="h4" color="textSecondary">
+        Utilize the below links to donate to various movements and organizations
+        related to Black Lives Matter.
+      </Typography>
+      <a target="_html" href="https://www.gofundme.com/f/georgefloyd">
+        George Floyd Memorial Fund
+      </a>
+    </div>
+  );
 };
 
 export default DonatePage;
